cbbef19f29e076254a547e4c383a19e2a1490b22
Vishakha Gautam <156678713+Vishakha2431@users.noreply.github.com>
2026-04-23 13:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+		let freq = {};
+
+	// count frequency
+	for (let i = 0; i < str.length; i++) {
+		let ch = str.charAt(i);
+		freq[ch] = (freq[ch] || 0) + 1;
+	}
+
+	// find first non-repeating
+	for (let i = 0; i < str.length; i++) {
+		let ch = str.charAt(i);
+		if (freq[ch] === 1) {
+			return ch;
+		}
+	}
+
+	return null;
 }
-const input = prompt("Enter a string");
+ const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
